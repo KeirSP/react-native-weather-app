80547aa52c90864832485acc5c3b04fdd34b17a2
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-07-11 22:36:36 +0100

    added styling

--- App.js
+++ App.js
@@ -8,6 +8,14 @@ export default function App() {
       <Text style={[styles.textStyle, styles.largeText]}>San Francisco</Text>
       <Text style={[styles.textStyle, styles.smallText]}>Light Cloud</Text>
       <Text style={[styles.textStyle, styles.largeText]}>32 Degrees</Text>  
+
+      <TextInput
+        autoCorrect = {false}
+        placeholder = "Search any city"
+        placeholderTextColor = "white"
+        style = {styles.textInput}
+        clearButtonMode = "always"
+       />
     </View>
   );
 }
@@ -35,5 +43,14 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: "white",
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center'
   }
 });
